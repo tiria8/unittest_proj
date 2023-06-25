e146099f6eedca7696a0ff7307a8aef0149448d2
daren <titadarenka@gmail.com>
2023-06-25 20:12:48 +0300

    fixed tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,12 @@
+import unittest
 from utils import arrs
 
+class TestGet(unittest.TestCase):
+    def test_get(self):
+        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(arrs.get([], 1, "test"), "test")
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
-
-
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+class TestSlice(unittest.TestCase):
+    def test_slice(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
